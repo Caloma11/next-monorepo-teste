1ec7acef74fb52ae7f47dc3557a364c6bcea4d78
ricardo-fnd <ricardodiask9@outlook.com>
2022-04-15 15:13:44 +0100

    removed node_modules

--- web-app/next.config.js
+++ web-app/next.config.js
@@ -1,6 +1,15 @@
 /** @type {import('next').NextConfig} */
+
 const nextConfig = {
   reactStrictMode: true,
+  async rewrites() {
+    return [
+      {
+        source: '/login/:path*',
+        destination: `http://localhost:3001/login/:path*`,
+      },
+    ]
+  },
 }
 
-module.exports = nextConfig
+module.exports = nextConfig;
