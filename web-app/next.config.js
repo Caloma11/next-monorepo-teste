6e2fc24a91d065e04b21620bf5dc63700cf99766
ricardo-fnd <ricardodiask9@outlook.com>
2022-04-15 15:54:54 +0100

    change destination url

--- web-app/next.config.js
+++ web-app/next.config.js
@@ -5,6 +5,10 @@ const nextConfig = {
   async rewrites() {
     return [
       {
+        source: '/login',
+        destination: `https://login-app-steel.vercel.app/login`,
+      },
+      {
         source: '/login/:path*',
         destination: `https://login-app-steel.vercel.app/login/:path*`,
       },
