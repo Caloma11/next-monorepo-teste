3d77532512fe5f06ceaad82b39db2609d8d679cc
ricardo-fnd <ricardodiask9@outlook.com>
2022-04-19 11:31:03 +0100

    add storybook & cypress

---
+++ stories/Button.stories.jsx
@@ -0,0 +1,47 @@
+import React from 'react';
+import { withDesign } from 'storybook-addon-designs';
+import { Button } from './Button';
+
+// More on default export: https://storybook.js.org/docs/react/writing-stories/introduction#default-export
+export default {
+  title: 'Example/Button',
+  component: Button,
+  // More on argTypes: https://storybook.js.org/docs/react/api/argtypes
+  argTypes: {
+    backgroundColor: { control: 'color' },
+  },
+  decorators: [withDesign]
+};
+
+// More on component templates: https://storybook.js.org/docs/react/writing-stories/introduction#using-args
+const Template = (args) => <Button {...args} />;
+
+export const Primary = Template.bind({});
+// More on args: https://storybook.js.org/docs/react/writing-stories/args
+Primary.args = {
+  primary: true,
+  label: 'Button',
+};
+
+Primary.parameters = {
+  design: {
+    type: 'figma',
+    url: 'https://www.figma.com/file/2stLv9IQlVm4lcQWckRnPX/Login-V2-2022?node-id=209%3A7729'
+  }
+}
+export const Secondary = Template.bind({});
+Secondary.args = {
+  label: 'Button',
+};
+
+export const Large = Template.bind({});
+Large.args = {
+  size: 'large',
+  label: 'Button',
+};
+
+export const Small = Template.bind({});
+Small.args = {
+  size: 'small',
+  label: 'Button',
+};
