5518e8c4edfcd6dd7a464ebf37c74c7a80fe825a
ricardo-fnd <ricardodiask9@outlook.com>
2022-04-19 16:32:03 +0100

    force login deploy to run github action

--- packages/login/cypress/integration/hello.spec.js
+++ packages/login/cypress/integration/hello.spec.js
@@ -2,6 +2,6 @@ describe('Landing', () => {
 
     it('checks for login message', () => {
         cy.visit('http://localhost:3001')
-        cy.contains('cenas');
+        cy.contains('LOGIN');
     })
 })
